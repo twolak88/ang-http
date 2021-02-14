cb5f7b9ad0e0ce490103a3cc3c21f7e4f9060de6
twolak88 <twolak88@gmail.com>
2021-02-14 19:07:59 +0100

    GETting Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -25,9 +27,18 @@ export class AppComponent {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get(
+      'https://ang-http-backend-01-default-rtdb.firebaseio.com/posts.json'
+    ).subscribe(posts => {
+      console.log(posts);
+    });
   }
 }
